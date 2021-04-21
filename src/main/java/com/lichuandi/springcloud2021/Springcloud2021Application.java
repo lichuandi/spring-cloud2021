04784680543fd65b05509f21be8d980ac4114b3a
lichuandi <your_email@mail.com>
2021-04-21 19:48:52 +0800

    第一次提交代码

---
+++ src/main/java/com/lichuandi/springcloud2021/Springcloud2021Application.java
@@ -0,0 +1,13 @@
+package com.lichuandi.springcloud2021;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Springcloud2021Application {
+
+    public static void main(String[] args) {
+        SpringApplication.run(Springcloud2021Application.class, args);
+    }
+
+}
